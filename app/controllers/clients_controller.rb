b39b497dc3c73ec4d93a43ae2238c4a29820e065
sandym <91996681+sandyml@users.noreply.github.com>
2023-01-25 20:44:07 -0500

    Done

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,13 @@
 class ClientsController < ApplicationController
+rescue_from ApplicationRecord::RecordNotFound, with: :not_found_response
 
  def show
   render json: Client.find(params[:id])
  end
+ 
+ private
 
+ def not_found_response
+  render json: { error: "Client not found" }, status: :not_found
+ end
 end
